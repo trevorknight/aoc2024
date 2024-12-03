c0a3f070438396ecd331a98b2a001e99026f9b26
Trevor Knight <TrevorKnight@gmail.com>
2024-12-03 17:52:56 -0500

    Day 2, part 2

--- 02/day_2.py
+++ 02/day_2.py
@@ -1,19 +1,23 @@
 def pos(x):
     return x > 0
 
-def safe_report(report):
-    values = [int(v) for v in report]
+def is_safe(values):
     differences = [values[i + 1] - values[i] for i in range(len(values) - 1)]
     v0_pos = pos(differences[0])
-    is_safe = all(0 < abs(v) <= 3 and pos(v) == v0_pos for v in differences)
-    # print(values, differences, is_safe)
-    return is_safe
+    return all(0 < abs(v) <= 3 and pos(v) == v0_pos for v in differences)
 
+def create_dropouts(values):
+    yield values
+    for i in range(0, len(values)):
+        yield values[:i] + values[i+1:]
+
+def safe_report(report):
+    values = [int(v) for v in report]
+    return any(is_safe(subset) for subset in create_dropouts(values))
 
 with open("input.txt", "r") as f:
     raw_lines = f.readlines()
 
 reports = (line.strip().split(' ') for line in raw_lines)
-
 safe_reports = sum(safe_report(report) for report in reports)
 print(safe_reports)
