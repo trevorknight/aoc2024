0eee61724df3db52a801a54ac4b5fc5bc159fd25
Trevor Knight <TrevorKnight@gmail.com>
2024-12-03 15:57:22 -0500

    Update day 1 script to by more pythonic

--- 01/day_1.py
+++ 01/day_1.py
@@ -4,9 +4,9 @@ with open('input.txt', 'r') as f:
     raw_lines = f.readlines()
 
 
-a, b = zip(*[line.strip().split() for line in raw_lines])
+a, b = zip(*(line.strip().split() for line in raw_lines))
 
-a, b = sorted(map(int, a)), sorted(map(int, b))
+a, b = sorted(int(v) for v in a), sorted(int(v) for v in b)
 
 if (len(a) != len(b)):
   print('Unequal lists of numbers somehow.')
@@ -20,16 +20,23 @@ for i in range(len(a)):
 print('Cumulative difference: ', cumulative_difference)
 
 similarity_score = 0
-b_index = 0
 
+#  Version 1
+# b_index = 0
+# for a_val in a:
+#   b_count = 0
+#   while b_index < len(b) and b[b_index] < a_val:
+#      b_index += 1
+#   while b_index < len(b) and b[b_index] == a_val:
+#     #  print(a_val, "found")
+#      b_count += 1
+#      b_index += 1
+#   similarity_score += a_val * b_count
+
+# Version 2
+from collections import Counter
+b_counter = Counter(b)
 for a_val in a:
-  b_count = 0
-  while b_index < len(b) and b[b_index] < a_val:
-     b_index += 1
-  while b_index < len(b) and b[b_index] == a_val:
-    #  print(a_val, "found")
-     b_count += 1
-     b_index += 1
-  similarity_score += a_val * b_count
+   similarity_score += a_val * b_counter[a_val]
 
 print('Similarity score', similarity_score)
