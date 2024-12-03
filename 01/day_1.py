7bb14867b54e18395fcbdf1ac3103d7bc5b240a9
Trevor Knight <TrevorKnight@gmail.com>
2024-12-03 16:00:22 -0500

    More tweaks

--- 01/day_1.py
+++ 01/day_1.py
@@ -3,7 +3,6 @@ import sys
 with open('input.txt', 'r') as f:
     raw_lines = f.readlines()
 
-
 a, b = zip(*(line.strip().split() for line in raw_lines))
 
 a, b = sorted(int(v) for v in a), sorted(int(v) for v in b)
@@ -12,16 +11,20 @@ if (len(a) != len(b)):
   print('Unequal lists of numbers somehow.')
   sys.exit()
 
-cumulative_difference = 0
+# Version 1
+# cumulative_difference = 0
 
-for i in range(len(a)):
-   cumulative_difference += abs(int(a[i]) - int(b[i]))
+# for i in range(len(a)):
+#    cumulative_difference += abs(int(a[i]) - int(b[i]))
+
+# Version 2
+cumulative_difference = sum(abs(int(a[i]) - int(b[i])) for i in range(len(a)))
 
 print('Cumulative difference: ', cumulative_difference)
 
-similarity_score = 0
 
 #  Version 1
+# similarity_score = 0
 # b_index = 0
 # for a_val in a:
 #   b_count = 0
@@ -36,7 +39,10 @@ similarity_score = 0
 # Version 2
 from collections import Counter
 b_counter = Counter(b)
-for a_val in a:
-   similarity_score += a_val * b_counter[a_val]
+# for a_val in a:
+#    similarity_score += a_val * b_counter[a_val]
+
+# Version 3
+similarity_score = sum(a_val * b_counter[a_val] for a_val in a)
 
 print('Similarity score', similarity_score)
