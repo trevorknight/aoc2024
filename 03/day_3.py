891274670c304674b4ce202f9bfaa20cb7cef1ec
Trevor Knight <TrevorKnight@gmail.com>
2024-12-03 18:58:43 -0500

    Day 3, Part 2 done

--- 03/day_3.py
+++ 03/day_3.py
@@ -3,7 +3,8 @@ import re
 with open("input.txt", "r") as f:
     input = f.read()
 
-
+# mul\(\d{1,3},\d{1,3}\)
+# mul\((\d{1,3}),(\d{1,3})\)
 regex = re.compile("mul\((\d{1,3}),(\d{1,3})\)")
 
 # Part 1, Version 1
@@ -16,10 +17,33 @@ regex = re.compile("mul\((\d{1,3}),(\d{1,3})\)")
 # total = sum(int(v.groups()[0]) * int(v.groups()[1]) for v in regex.finditer(input))
 
 # Part 1, Version 3
-total = sum(int(a) * int(b) for a, b in (v.groups() for v in regex.finditer(input)))
+# total = sum(int(a) * int(b) for a, b in (v.groups() for v in regex.finditer(input)))
 
-print(total)
+# Part 1, Version 4
+total = sum(int(v.group(1)) * int(v.group(2)) for v in regex.finditer(input))
 
 
-# mul\(\d{1,3},\d{1,3}\)
-# mul\((\d{1,3}),(\d{1,3})\)
+print('Part 1 total:', total)
+
+# Part 2
+
+mul_matches = [
+    (v.span()[0], int(v.group(1)) * int(v.group(2))) for v in regex.finditer(input)
+]
+do_regex = re.compile("do\(\)")
+do_matches = [(v.span()[0], True) for v in do_regex.finditer(input)]
+dont_regex = re.compile("don't\(\)")
+dont_matches = [(v.span()[0], False) for v in dont_regex.finditer(input)]
+
+all_matches = (v[1] for v in sorted(mul_matches + do_matches + dont_matches, key=lambda x: x[0]))
+
+do_add = True
+total = 0
+for value in all_matches:
+    if type(value) is bool:
+        do_add = value
+    elif do_add:
+      total += value
+
+print('Do and don\'t total:', total)
+        
