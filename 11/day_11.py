dac67a7a3810f80eac03c2090aa19464391a3a62
Trevor Knight <TrevorKnight@gmail.com>
2024-12-11 21:35:42 -0500

    Day 11, Part 2

--- 11/day_11.py
+++ 11/day_11.py
@@ -1,27 +1,46 @@
-NUM_BLINKS = 25
+from typing import Dict
+
+
+NUM_BLINKS = 75
 
 TEST = [int(v) for v in "125 17".split(" ")]
 INPUT = [int(v) for v in "2 77706 5847 9258441 0 741 883933 12".split(" ")]
 
 stones = INPUT
 
-for _ in range(NUM_BLINKS):
-  new_stones = []
-  for i, stone in enumerate(stones):
-    if stone == 0:
-      new_stones.append(1)
-      continue
-    str_stone = str(stone)
-    l = len(str_stone)
-    if len(str_stone) % 2 == 0:
-      a = int(str_stone[:l//2])
-      b = int(str_stone[l//2:])
-      new_stones.append(a)
-      new_stones.append(b)
-      continue
-    new_stones.append(stone * 2024)
-  stones = new_stones
+cache: Dict[int, Dict[int, int]] = {}
+# Gen #      0  1     2     3        4
+# Stones     0  1  2024 20 24  2 0 2 4
+# Cache  0: [1, 1,    1,    2,       4]
 
-print(stones)
-print(len(stones))
 
+def evolveStone(stone_value):
+    if stone_value == 0:
+        return [1]
+    str_stone = str(stone_value)
+    length = len(str_stone)
+    if length % 2 == 0:
+        a = int(str_stone[: length // 2])
+        b = int(str_stone[length // 2 :])
+        return [a, b]
+    return [stone_value * 2024]
+
+
+def getNumStonesAfterGens(start_value, num_gens):
+    if num_gens == 0:
+        return 1
+    if start_value in cache and num_gens in cache[start_value]:
+        # print(f"Found {start_value=} {num_gens=} in cache: cache[start_value][num_gens]")
+        return cache[start_value][num_gens]
+    new_stones = evolveStone(start_value)
+    num_descendants = sum([getNumStonesAfterGens(v, num_gens - 1) for v in new_stones])
+    # print(f"New entry {start_value=} {num_gens=} in cache: {num_descendants=}")
+    cache.setdefault(start_value, {})[num_gens] = num_descendants
+    return num_descendants
+
+
+total = sum([getNumStonesAfterGens(value, NUM_BLINKS) for value in INPUT])
+
+
+# print(cache)
+print(total)
